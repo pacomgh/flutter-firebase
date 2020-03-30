77186aa3b37feeac04d87df0081ab5a88f6efc41
pacomgh <paco.mgh@gmail.com>
2020-03-30 16:26:40 -0600

    add details(title and card) in detailpage

--- flutter_firebase_backend/lib/DetailsPage.dart
+++ flutter_firebase_backend/lib/DetailsPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -18,6 +19,41 @@ class _DetailPageState extends State<DetailPage> {
       appBar: new AppBar(
         title: new Text("Details Page"),
         backgroundColor: Colors.green,
+      ),
+      body: new ListView(children: <Widget>[
+          new Container(
+            margin: EdgeInsets.all(5.0),
+            child: new Card(
+              elevation: 9.0,
+              child: new Container(
+                padding: EdgeInsets.all(10.0),
+                child: new Row(
+                  children: <Widget>[
+                    new CircleAvatar(
+                      child: new Text(widget.snapshot.data["title"][0]),
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white,
+                    ),
+                    new SizedBox(width: 10.0,),
+                    new Text(widget.snapshot.data["title"],
+                      style: TextStyle(fontSize: 15.0, color: Colors.green),)
+                  ],
+                ),
+              ),
+            ),
+          ),
+          new Container(
+            margin: EdgeInsets.all(10.0),
+            child: new ClipRRect(
+              borderRadius: new BorderRadius.circular(10.0),
+              child: new Image.network(widget.snapshot.data["url"],
+              height: 200.0,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,),
+
+            ),
+          ),
+        ],
       ),
     );
   }
